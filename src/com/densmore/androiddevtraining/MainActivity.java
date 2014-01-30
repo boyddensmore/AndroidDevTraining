2a0101713bed2a2ae40566de98f625e66b033294
Boyd Densmore <boyddensmore@gmail.com>
2014-01-30 11:09:55 -0700

    Changed API, played with styles + icons
    Changed min API from 8 to 11 to avoid issues with Themes, removed
    specific themes for API 14+, played with theme settings in v11
    styles.xml, added Search action icon

--- src/com/densmore/androiddevtraining/MainActivity.java
+++ src/com/densmore/androiddevtraining/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -23,6 +24,21 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            sendMessage(this.getCurrentFocus());
+	            return true;
+	        case R.id.action_settings:
+	            //openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
 	public void sendMessage(View view) {
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
 		
